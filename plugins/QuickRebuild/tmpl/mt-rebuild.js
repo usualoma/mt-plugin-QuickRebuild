5ef8cd86cb87dc80687f3431eeb08a370ec7352f
taiju <higashi@taiju.info>
2016-08-02 15:53:53 +0900

    Fix infinite loop

--- plugins/QuickRebuild/tmpl/mt-rebuild.js
+++ plugins/QuickRebuild/tmpl/mt-rebuild.js
@@ -712,7 +712,7 @@ ToIMT.prototype.list_blogs = function(sites, onListed) {
                 }));
             });
 
-            if (parseInt(data.result.page, 10) !== parseInt(data.result.page_max, 10)) {
+            if (parseInt(data.result.page_max, 10) > 0 && parseInt(data.result.page, 10) !== parseInt(data.result.page_max, 10)) {
                 inner(parseInt(data.result.page, 10) + 1);
             }
             else {
